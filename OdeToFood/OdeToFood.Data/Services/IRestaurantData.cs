440742869e8c04681324189a534f322f2f048729
Marie Kelling <mkelling@schumacherhomes.com>
2020-04-28 14:22:17 -0400

    basic MVC functionality implemented

---
+++ OdeToFood/OdeToFood.Data/Services/IRestaurantData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OdeToFood.Data.Models;
+
+namespace OdeToFood.Data.Services
+{
+    public interface IRestaurantData
+    {
+        /* Different operations we want to perform on the data source. CRUD */
+
+        IEnumerable<Restaurant> GetAll();
+    }
+}
